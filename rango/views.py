015cfe802040cbb2ad019502614dee2d5850ee4c
Gregor Forbes <2317756f@student.gla.ac.uk>
2019-02-06 17:14:58 +0000

    Completed Chapter 4 of Tango_With_Django  Changes to be committed: 	modified:   .idea/workspace.xml 	new file:   media/cat.jpg 	modified:   rango/__pycache__/__init__.cpython-37.pyc 	modified:   rango/__pycache__/admin.cpython-37.pyc 	modified:   rango/__pycache__/models.cpython-37.pyc 	modified:   rango/__pycache__/urls.cpython-37.pyc 	modified:   rango/__pycache__/views.cpython-37.pyc 	modified:   rango/migrations/__pycache__/__init__.cpython-37.pyc 	modified:   rango/urls.py 	modified:   rango/views.py 	new file:   static/images/rango.jpg 	new file:   static/images/rango1.jpg 	modified:   tango_with_django_project/__pycache__/__init__.cpython-37.pyc 	modified:   tango_with_django_project/__pycache__/settings.cpython-37.pyc 	modified:   tango_with_django_project/__pycache__/urls.cpython-37.pyc 	modified:   tango_with_django_project/settings.py 	modified:   tango_with_django_project/urls.py 	new file:   templates/rango/about.html 	new file:   templates/rango/index.html

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,16 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("Rango Says hey there partner")
+
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
+
 
 def about(request):
-    return HttpResponse("Rango Says here is the about page")
+
+    # return HttpResponse('Rango Says here is the about page')
+    about_dict ={'yourname' : "Gregor"}
+    return render(request, 'rango/about.html', context=about_dict)
 
