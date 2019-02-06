96a20d73eb9e4dfc4a88a3b759c3f2f793d9dc52
Gregor Forbes <2317756f@student.gla.ac.uk>
2019-02-06 14:39:12 +0000

    cleaned up views and urls

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
-
 
 def index(request):
     return HttpResponse("Rango Says hey there partner")
+
+def about(request):
+    return HttpResponse("Rango Says here is the about page")
+
